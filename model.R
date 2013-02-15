c97389f40763dec825b55887fde1e828862dcaa1
Gregg Whitworth <whitwort@gmail.com>
2013-02-15 16:21:39 -0500

    Added more comments

--- model.R
+++ model.R
@@ -5,21 +5,21 @@ library(deSolve)
 parameters <- c(
     kon   = 1000
   , koff  = 10
-  )
+)
 
 # Initial values of state variables (represent concentrations)
 state <- c(
-    A   = 0.001
+  A   = 0.001
   , B   = 0.01
   , AB  = 0
-  )
+)
 
 # Time window and step size
 time <- c(
     start = 0
   , end   = 1
   , step  = 0.01
-  )
+)
 
 # deSolve functional interface; t is the model time passed by the library
 model <- function(t, state, parameters) {
@@ -27,7 +27,8 @@ model <- function(t, state, parameters) {
   # We'll bind state and parameter variables to clean up our model code block
   with(as.list(c(state,parameters)), {
     
-    # This function returns an ordered list of rate of change calculations
+    # This function returns an ordered list of rate of change calculations:
+    # the order should match that of the state vector
     return(list(c(
       
         dA  <- (koff * AB) - (kon * A * B)
@@ -43,18 +44,21 @@ model <- function(t, state, parameters) {
 # define the solver that we want to use
 solver <- ode
 
-# Page elements describing this model
+# Header element describing this model
 headerText  <- "Bimolecular Binding Model"
+
+# Footer with some extra text.  (this is markdown code)
 footerText  <- "[Source code](https://github.com/whitwort/bindingModel) available on github."
 
-# Little dev function to run the model in an interactive session (not run by the server)
+# Little development helper function to run the model in an interactive session
+# (not used by the server).  To test your model, do `r <- runModel()`.
 runModel <- function() {
   result <- solver(  
-          y     = state
-        , times = seq(time["start"], time["end"], by = time["step"])
-        , func  = model
-        , parms = parameters
-      )
+      y     = state
+    , times = seq(time["start"], time["end"], by = time["step"])
+    , func  = model
+    , parms = parameters
+  )
   
   print(head(result))
   print(tail(result))
