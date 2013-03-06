723e5804c5b4bba617812e2c97dbb371b026d11b
Gregg Whitworth <whitwort@gmail.com>
2013-03-06 17:45:08 -0500

    Removed duplicates

--- tests/dirtify.R
+++
@@ -1,9 +0,0 @@
-# This is a total hack that dirties ui.R and server.R so that they are reloaded
-# by the framework.
-dirtify <- function(files = c("./ui.R", "./server.R")) {
-  for (file in files) {
-    system( paste("touch", file) )
-  }
-}
-
-dirtify()
